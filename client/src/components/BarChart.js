103e4e9a1f8befca56b7335f63c5bb16146ab881
hqluan <157230085+hqluan@users.noreply.github.com>
2024-04-23 21:26:57 -0500

    Update BarChart.js

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -3,11 +3,11 @@ import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 
 const BarChartComponent = ({ data }) => { 
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <ResponsiveContainer width="90%" height="90%">
       <BarChart
         width={50}
         height={20}
-        data={data}
+        data={data} 
         margin={{
           top: 5,
           right: 30,
@@ -18,12 +18,13 @@ const BarChartComponent = ({ data }) => {
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis domain={[0, 70000]}/>
-        <Tooltip />
+        <Tooltip labelFormatter={(value) => `${value}：Crime Count`} />
         <Legend />
         <Bar dataKey="crime_count" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
       </BarChart>
     </ResponsiveContainer>
   );
 }
+
 
 export default BarChartComponent;
