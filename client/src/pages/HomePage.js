d12b1a581472ded4fbae9b64453155f64469875e
hqluan <157230085+hqluan@users.noreply.github.com>
2024-04-23 21:37:14 -0500

    Update HomePage.js
    v2- most current homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,29 @@
 import { useEffect, useState } from "react";
-import { Container, Divider, Link } from "@mui/material";
+import { Container, Divider } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import Carousel from "../components/Carousel";
+import { SliderData } from "../components/Data";
+import "../app.css";
+import {
+  APIProvider,
+  Map,
+  Pin,
+  AdvancedMarker,
+} from "@vis.gl/react-google-maps";
 
 import LazyTable from "../components/LazyTable";
 const config = require("../config.json");
 
 export default function HomePage() {
   const [appAuthor, setAppAuthor] = useState("");
+  const position = [
+    { lat: 40.6970192, lng: -74.3093451 },
+    { lat: 40.57649571266335, lng: -73.84837669505295 },
+    { lat: 40.577390058334586, lng: -74.13834755054633 },
+    { lat: 40.56184964143356, lng: -73.88796264178508 },
+    { lat: 40.611295809274296, lng: -74.06038542143051 },
+    { lat: 40.60596913792262, lng: -74.05605235768698 },
+  ];
 
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/author/name`)
@@ -23,7 +40,7 @@ export default function HomePage() {
       field: "neighborhood",
       headerName: "Neighborhood",
       renderCell: (row) => (
-        <Link
+        <a
           href={`https://en.wikipedia.org/wiki/${row.neighborhood.replace(
             " ",
             "_"
@@ -32,40 +49,85 @@ export default function HomePage() {
           rel="noopener noreferrer"
         >
           {row.neighborhood}
-        </Link>
+        </a>
       ),
     },
     {
       field: "neighborhood_group",
       headerName: "Neighborhood Group",
       renderCell: (row) => (
-        <Link
+        <a
           href={`https://en.wikipedia.org/wiki/${row.neighborhood_group}`}
           target="_blank"
           rel="noopener noreferrer"
         >
           {row.neighborhood_group}
-        </Link>
+        </a>
       ),
-    },
-    {
-      headerName: "Star Host",
-    },
+    }
   ];
 
   return (
     <Container>
+      {/* Image slider*/}
+      <div>
+        {" "}
+        <Carousel images={SliderData} />
+      </div>
+      <Container class="video-test-container">
+        <div>
+          <section id="video-test-container">
+            <div>
+              <iframe
+                class="video-container"
+                id="first viedo"
+                width="450"
+                height="374"
+                src="https://www.youtube.com/embed/yD-CPM02GF0"
+                title="NEW YORK - Cinematic Travel"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div>
+              <iframe
+                class="video-container"
+                width="450"
+                height="374"
+                src="https://www.youtube.com/embed/v86r22gGvRA?&si=nxA2Uy3KlPnz9a6h&amp;start=25"
+                title="I LIKE ME BETTER WITH YOU"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </section>
+        </div>
+      </Container>
+
       {/* list saftest neighbors */}
       <h2>Top 5 Safetest Neighbors:&nbsp;</h2>
+      <p>
+        We slected poppular and the safetest neighborhoods for you! Check detailed listings <NavLink to="/recommendations">here</NavLink>.
+      </p>
       <LazyTable
         route={`http://${config.server_host}:${config.server_port}/top_5_neighbors`}
         columns={top5Neighbors}
         defaultPageSize={5}
-        rowsPerPageOptions={[5, 10]}
+        rowsPerPageOptions={[5]}
       />
       <Divider />
+      <Container class="map-container">
+        <APIProvider apiKey="AIzaSyCYHonDUWTua7kF363vtOtnp1aYtoRrvBM">
+          <Map zoom={12} center={position[0]}>
+          </Map>
+        </APIProvider>
+      </Container>
       {/* app authors */}
-      <p>{appAuthor}</p>
+      <p style={{ fontWeight: 'bold' }}>{appAuthor}</p>
     </Container>
   );
 }
