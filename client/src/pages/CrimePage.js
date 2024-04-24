8961c0f581b5eaddfa8c57988879d13a8ab8ac6f
Rita Zhang <ritazhang@Ritas-MacBook-Pro.local>
2024-04-24 01:26:09 -0700

    typo

--- client/src/pages/CrimePage.js
+++ client/src/pages/CrimePage.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
-import "../app.css";
+import "./app.css";
 import {
   Button,
   Checkbox,
   Container,
   FormControlLabel,
   Grid,
+  Link,
+  Slider,
+  TextField,
   Select,
   MenuItem,
   FormControl,
   InputLabel,
+  Typography,
 } from "@mui/material";
 import {
   ResponsiveContainer,
@@ -17,21 +21,29 @@ import {
   Bar,
   XAxis,
   YAxis,
+  RadarChart,
+  Radar,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
   Legend,
 } from "recharts";
+import { DataGrid } from "@mui/x-data-grid";
 import BarChartComponent from "../components/BarChart";
 import { useSearchParams } from "react-router-dom";
+// import HostListing from "../components/HostListing";
 
 const config = require("../config.json");
 
 //query neighborhood group, nb, accommodate, days, room-type, bed, bath
 export function CrimePage() {
+  const [pageSize, setPageSize] = useState(10);
 
   //state hooks for fetching
   const [crimeData, setCrimeData] = useState([]);
+  const [barchartData, setbarchartData] = useState([]);
   const [neighborhoodData, setNeighborhoods] = useState([]);
   const [demoData, setdemoData] = useState([]);
-  const [groupCrimeData, setGroupCrimeData] = useState([]);
 
   //necessary filters
   const [searchParams] = useSearchParams();
@@ -70,6 +82,29 @@ export function CrimePage() {
     }
   };
 
+  const groupCrimeData = [
+    {
+      name: "Queens",
+      crime_count: 50965,
+    },
+    {
+      name: "Bronx",
+      crime_count: 49361,
+    },
+    {
+      name: "Manhattan",
+      crime_count: 53321,
+    },
+    {
+      name: "Staten Island",
+      crime_count: 11194,
+    },
+    {
+      name: "Brooklyn",
+      crime_count: 60379,
+    },
+  ];
+
   //fetch recommendation listing base on filters
   const search = () => {
     console.log("Search initiated with filters:", {
@@ -80,19 +115,6 @@ export function CrimePage() {
       neighborhoodGroup,
       neighborhood,
     });
-
-  fetch(`http://${config.server_host}:${config.server_port}/crime/neighborhood_group`)
-    .then((res) => res.json())
-    .then((resJson) => {
-      const featuredListings = resJson.map((row) => ({
-        id: row.group_crime_id, 
-        ...row,
-      }));
-      setGroupCrimeData(featuredListings);
-    })
-    .catch(error => {
-      console.error('There was a problem with your fetch operation:', error);
-  });
 
     fetch(
       `http://${config.server_host}:${
@@ -109,6 +131,14 @@ export function CrimePage() {
         }));
 
         setCrimeData(crimeDataJson);
+
+        // const newData = crimeDataJson.map((row) => ({
+        //   type: row.ofns_type,
+        //   value: row.offense_count
+        // }));
+        // console.log(newData);
+        // setbarchartData(newData);
+        // console.log("bcd", barchartData);
       })
       .catch((error) => {
         console.error("Failed to fetch recommendation", error);
@@ -129,6 +159,14 @@ export function CrimePage() {
         }));
 
         setdemoData(demoDataJson);
+
+        // const newData = crimeDataJson.map((row) => ({
+        //   type: row.ofns_type,
+        //   value: row.offense_count
+        // }));
+        // console.log(newData);
+        // setbarchartData(newData);
+        // console.log("bcd", barchartData);
       })
       .catch((error) => {
         console.error("Failed to fetch recommendation", error);
@@ -145,7 +183,7 @@ export function CrimePage() {
     <div className="container">
       <div
         className="lower-left-table"
-        style={{ width: "100%", height: "700px" }}
+        style={{ width: "100%", height: "400px" }}
       >
         <h3>Neighborhood Group Crime Chart </h3>
         <div>
@@ -184,11 +222,8 @@ export function CrimePage() {
         </ResponsiveContainer>
       </div>
 
-      <Container class="upper-left-table">
+      <Container>
         <h2>Filters</h2>
-        <p>
-          Select neighborhood group and neighborhood to see crime statistics.
-        </p>
         <Grid container spacing={6}>
           <Grid item xs={3}>
             <FormControl fullWidth>
@@ -225,16 +260,13 @@ export function CrimePage() {
               </Select>
             </FormControl>
           </Grid>
-
-          <Grid item xs={3} style={{ display: "flex", alignItems: "flex-end" }}>
-            <Button onClick={() => search()} style={{ marginLeft: "auto" }}>
-              Search
-            </Button>
-          </Grid>
         </Grid>
-        <div class="image-container">
-          <img src="https://images.pexels.com/photos/9364799/pexels-photo-9364799.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        </div>
+        <Button
+          onClick={() => search()}
+          style={{ left: "50%", transform: "translateX(-50%)" }}
+        >
+          Search
+        </Button>
       </Container>
     </div>
   );
