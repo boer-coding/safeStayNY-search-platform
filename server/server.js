aa2557996e17075f1d9d6e3df807fa8451f4e35c
Boer Liu <liuboer@seas.upenn.edu>
2024-09-23 12:39:57 -0700

    final

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const config = require("./config");
+const config = require("./config");
 const routes = require("./routes");
 
 const app = express();
@@ -9,9 +9,7 @@ app.use(
     origin: "*",
   })
 );
-// app.get('/', (req, res) => {
-//   res.send('Welcome to SafeStayNY API');
-// });
+
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
 app.get("/author/:type", routes.author);
@@ -26,10 +24,10 @@ app.get("/crime", routes.crime);
 app.get("/crime/neighborhood_group",routes.neighborhood_group_crime);
 app.get("/crimeDemographic", routes.crimeDemographic);
 
-const port = process.env.PORT || 8080; // Use Heroku's provided port or default to 8080
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(config.server_port, () => {
+  console.log(
+    `Server running at http://${config.server_host}:${config.server_port}/`
+  );
 });
 
 module.exports = app;
