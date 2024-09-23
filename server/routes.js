adaa7049f712ba679c82115d8bf80863106fe270
Boer Liu <liuboer@seas.upenn.edu>
2024-09-23 11:18:31 -0700

    Fix Heroku deployment

--- server/routes.js
+++ server/routes.js
@@ -3,16 +3,16 @@ const config = require("./config.json");
 
 /* Creates MySQL connection using database credential provided in config.json */
 const connection = mysql.createConnection({
-  // host: config.rds_host,
-  // user: config.rds_user,
-  // password: config.rds_password,
-  // port: config.rds_port,
-  // database: config.rds_db,
-  host: process.env.RDS_HOST,
-  user: process.env.RDS_USER,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB,
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db,
+  // host: process.env.RDS_HOST,
+  // user: process.env.RDS_USER,
+  // password: process.env.RDS_PASSWORD,
+  // port: process.env.RDS_PORT,
+  // database: process.env.RDS_DB,
   multipleStatements: true,
 });
 connection.connect((err) => {
